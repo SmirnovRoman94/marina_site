c6b19abc648707aa723e3b7647ce3535affebc09
Roman Smirnov <smirnow3217831@gmail.com>
2024-03-16 13:54:44 +0300

    fix register

--- backend/app/Http/Requests/auth/AuthStoreRequest.php
+++ backend/app/Http/Requests/auth/AuthStoreRequest.php
@@ -24,7 +24,7 @@ class AuthStoreRequest extends FormRequest
         return [
             'name' => 'required|string|min:3',
             'surname' => 'required|string|min:3',
-            'patromic' => 'required|string|min:3',
+            'patronomic' => 'required|string|min:3',
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string|min:8'
         ];
