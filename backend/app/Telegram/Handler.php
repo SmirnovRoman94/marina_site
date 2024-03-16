4235b85797bb2e1108dc4bd37afb9f67b104c486
Roman Smirnov <smirnow3217831@gmail.com>
2024-03-16 16:11:28 +0300

    fix handler telegram

--- backend/app/Telegram/Handler.php
+++ backend/app/Telegram/Handler.php
@@ -19,14 +19,11 @@ class Handler extends WebhookHandler
 
     static public function sendNewOrder($file, $patient)
     {
-        $request = new Request(['name' => $file]);
-        $url = FileController::index($request);
-
-        Telegraph::message("Новый Заказ {$url}")
+        Telegraph::message("Новый Заказ")
             ->keyboard(Keyboard::make()->buttons([
                 Button::make("👌Оплата прошла")->action("sendPay"),
                 Button::make("🤚 Оплата не прошла")->action("noSendPay"),
-            ])->chunk(1))->photo("https://yandex.ru/images/search?img_url=https%3A%2F%2Fwww.russiadiscovery.ru%2Fstorage%2Fresolutions%2Fbig%2Fdescription_details%2F6026%2Fimg_64b5a0f6764e6.jpg&lr=2&nl=1&pos=13&rpt=simage&source=morda&text=%D0%9A%D0%B0%D1%80%D0%B5%D0%BB%D0%B8%D1%8F%20%D0%97%D0%B8%D0%BC%D0%BE%D0%B9")->send();
+            ])->chunk(1))->photo("https://34fih34j.org/api/files?name={$file}")->send();
     }
 
     public function sendPay(): void
