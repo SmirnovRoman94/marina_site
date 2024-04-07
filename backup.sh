4a48e6c4afb891d0c413041502c716d0b087432d
Roman Smirnov <smirnow3217831@gmail.com>
2024-04-07 23:30:59 +0300

    fix docker-compose

--- backup.sh
+++ backup.sh
@@ -4,4 +4,4 @@ MYSQL_USER=user
 MYSQL_PASSWORD=password
 BACKUP_DIR=/backup
 # Импортируем резервную копию базы данных
-mysql -u $DB_USER -p$DB_PASS $DB_NAME < $BACKUP_DIR/backup.sql
+mysql -u $DB_USER -p$DB_PASS $DB_NAME > $BACKUP_DIR/backup.sql
