fb5daedf76baad37f986289c0d3b4218716fdda8
Roman Smirnov <smirnow3217831@gmail.com>
2024-04-07 23:42:20 +0300

    fix

--- backup.sh
+++
@@ -1,7 +0,0 @@
-MYSQL_ROOT_PASSWORD=root
-MYSQL_DATABASE=database
-MYSQL_USER=user
-MYSQL_PASSWORD=password
-BACKUP_DIR=/backup
-# Импортируем резервную копию базы данных
-mysql -u $DB_USER -p$DB_PASS $DB_NAME > $BACKUP_DIR/backup.sql
